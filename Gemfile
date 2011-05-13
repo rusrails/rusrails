977007ca1fd996ea0dfcf908c772e37945be8b5e
Mik_Die <MikDiet@gmail.com>
2011-05-13 11:45:41 +0800

    initial commit

---
+++ Gemfile
@@ -0,0 +1,13 @@
+source 'http://rubygems.org'
+
+gem 'rails', '3.0.7'
+gem 'mysql2', '= 0.2.7'     # version needed for rails 3.0
+gem 'devise'
+
+gem 'capistrano'
+
+group :development, :test do
+  gem "rspec-rails"
+  gem "cucumber-rails"
+  gem 'webrat'
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Urrr::Application.config.secret_token = '80e6f32ffdc5ca8f5061599f3dccc215e32336d805c3df86377e2b2ecd4b3a07a16e00609f492115082135b1f636fd87a579fc495e5c4c46db6bf29a98b40398'
