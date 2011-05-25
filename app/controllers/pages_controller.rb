c96d50fe4ae85fb9343c2b0bbbeca713924c7ef0
Mik_Die <MikDiet@gmail.com>
2011-05-25 21:33:16 +0800

    added action pages#index

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def index
+    @homepage = Page.matching("home") ||
+        Page.create(:name => "Homepage", :url_match => "home")
   end
 
   def show
--- spec/controllers/pages_controller_spec.rb
+++ spec/controllers/pages_controller_spec.rb
@@ -1,19 +1,32 @@
 require 'spec_helper'
 
 describe PagesController do
-
-  describe "GET 'index'" do
-    it "should be successful" do
-      get 'index'
-      response.should be_success
+  describe "GET index" do
+    let(:homepage){ mock_model(Page) }
+    before :each do
+      Page.stub(:matching).with("home").and_return homepage
+    end
+    
+    it "loads homepage if present" do
+      Page.should_receive(:matching)
+      get :index
+    end
+    
+    it "creates new empty homepage if it isn't present yet" do
+      Page.should_receive(:matching).with("home").and_return nil
+      Page.should_receive(:create)
+          .with(:name => "Homepage", :url_match => "home").and_return homepage
+      get :index
+    end
+    
+    it "gives homepage to the view" do
+      get :index
+      assigns[:homepage].should eq(homepage)
+    end
+    
+    it "render index template" do
+      get :index
+      response.should render_template("index")
     end
   end
-
-  describe "GET 'show'" do
-    it "should be successful" do
-      get 'show'
-      response.should be_success
-    end
-  end
-
 end
