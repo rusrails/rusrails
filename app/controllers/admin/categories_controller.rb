b25cb8fdcb3d288b647048b683bd29acb00edf15
Mik_Die <MikDiet@gmail.com>
2011-06-01 11:26:58 +0800

    created action new and create for admin/categories

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,12 +1,23 @@
+# encoding: utf-8
 class Admin::CategoriesController < Admin::IndexController
   def index
     @categories = Category.ordered
   end
   
   def new
+    @category = Category.new flash[:category]
   end
   
   def create
+    category = Category.new params[:category]
+    if category.save
+      flash[:notice] = "Категория создана"
+      redirect_to admin_categories_path
+    else
+      flash[:alert] = "Произошли ошибки: "+category.errors.full_messages*", "
+      flash[:category] = params[:category]
+      redirect_to new_admin_category_path
+    end
   end
   
   def edit
--- spec/controllers/admin/categories_controller_spec.rb
+++ spec/controllers/admin/categories_controller_spec.rb
@@ -39,5 +39,78 @@ describe Admin::CategoriesController do
         response.should render_template(:index)
       end
     end
+    
+    describe "GET 'new'" do
+      let(:category){mock_model(Category).as_new_record}
+      before :each do
+        Category.stub(:new).and_return category
+      end
+      
+      it "assigns @category as new category" do
+        Category.should_receive(:new)
+        get :new
+        assigns[:category].should == category
+      end
+      
+      it "creates new category from flash[:category]" do
+        Category.should_receive(:new).with "name"=>"Category 1", "url_match"=>"category-1"
+        post :new,nil,nil,{:category => {"name"=>"Category 1", "url_match"=>"category-1"}}
+      end
+      
+      it "renders new template" do
+        get :new
+        response.should render_template(:new)
+      end
+    end
+    
+    describe "POST 'create'" do
+      let(:category){mock_model(Category, :save => true).as_new_record.as_null_object}
+      before :each do
+        Category.stub(:new).and_return category
+      end
+      
+      it "creates new category" do
+        Category.should_receive(:new).with "name"=>"Category 1", "url_match"=>"category-1"
+        post :create, :category => {"name"=>"Category 1", "url_match"=>"category-1"}
+      end
+      
+      it "saves the category" do
+        category.should_receive :save
+        post :create
+      end
+      
+      context "when saving succesfull" do
+        it "sets flash[:notice]" do
+          post :create
+          flash[:notice].should=~ /.+/
+        end
+        
+        it "redirects to categories index" do
+          post :create
+          response.should redirect_to(admin_categories_path)
+        end
+      end
+      
+      context "when saving failed" do
+        before :each do
+          category.stub(:save).and_return false
+        end
+        
+        it "sets flash[:alert]" do
+          post :create
+          flash[:alert].should=~ /.+/
+        end
+        
+        it "sets flash[:category] with params[:category]" do
+          post :create, :category => {"name"=>"Category 1", "url_match"=>"category-1"}
+          flash[:category].should == {"name"=>"Category 1", "url_match"=>"category-1"}
+        end
+        
+        it "redirects to new category" do
+          post :create
+          response.should redirect_to(new_admin_category_path)
+        end
+      end
+    end
   end
 end
