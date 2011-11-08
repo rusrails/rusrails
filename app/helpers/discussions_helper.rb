0db771f565b4250e0984a23c553458146be3036b
Mikhail Dieterle <MikDiet@gmail.com>
2011-11-09 04:16:40 +0800

    admin name

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -2,7 +2,7 @@ module DiscussionsHelper
   def author_of(resource)
     author = resource.author
     case author
-    when Admin then "<b>#{author.name}</b>".html_safe
+    when Admin then "<span class='admin_name'>#{author.name}</span>".html_safe
     when User then author.name
     else "неизвестный"
     end
