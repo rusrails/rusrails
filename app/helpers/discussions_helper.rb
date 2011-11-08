28d74778b84cfc2b61b9282e2baf180e14c9cfea
Mikhail Dieterle <MikDiet@gmail.com>
2011-11-09 02:30:26 +0800

    names

--- app/helpers/discussions_helper.rb
+++ app/helpers/discussions_helper.rb
@@ -1,2 +1,10 @@
 module DiscussionsHelper
+  def author_of(resource)
+    author = resource.author
+    case author
+    when Admin then "<b>#{author.name}</b>".html_safe
+    when User then author.name
+    else "неизвестный"
+    end
+  end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,7 +5,7 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable
 
   # Setup accessible (or protected) attributes for your model
-  attr_accessible :email, :password, :password_confirmation, :remember_me
+  attr_accessible :email, :password, :password_confirmation, :remember_me, :name
 
   has_many :discussions, :as => :author
   has_many :says, :as => :author
