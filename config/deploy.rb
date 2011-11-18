ebd3360105fe96621fba3f77f90ea76844fadacc
Mikhail Dieterle <MikDiet@gmail.com>
2011-11-18 18:27:36 +0800

    deploy reciepe modified

--- config/deploy.rb
+++ config/deploy.rb
@@ -62,9 +62,11 @@ namespace :deploy do
   end
   # after "deploy:migrate", "deploy:seed_database"
 
+  set :unicorn_start_cmd, "rvm use 1.9.3 do bundle exec unicorn_rails -Dc #{unicorn_conf}"
+
   desc "Start application"
   task :start, :roles => :app do
-    run "#{unicorn_rails} -Dc #{unicorn_conf}"
+    run unicorn_start_cmd
   end
 
   desc "Stop application"
@@ -74,7 +76,7 @@ namespace :deploy do
 
   desc "Restart Application"
   task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_rails} -Dc #{unicorn_conf}"
+    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
   end
 
   task :restart_ts, :roles => :app do
