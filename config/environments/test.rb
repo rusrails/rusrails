a9898100637dfb7f90e34f30034cd8c0a8770853
Mikhail Dieterle <MikDiet@gmail.com>
2012-04-19 00:31:21 +0800

    release notes translates

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -39,4 +39,7 @@ Rusrails::Application.configure do
 
   # Allow pass debug_assets=true as a query parameter to load pages with unpackaged assets
   config.assets.allow_debugging = true
+
+  # Raise exception on mass assignment protection for Active Record models
+  config.active_record.mass_assignment_sanitizer = :strict
 end
