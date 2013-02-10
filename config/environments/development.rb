9d3abdf86fa007b8b05754f063f93a6ed617b4f9
Mikhail Dieterle <MikDiet@gmail.com>
2013-02-10 18:46:04 +0300

    adapt other markup

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -22,7 +22,7 @@ Rusrails::Application.configure do
   # Only use best-standards-support built into browsers
   config.action_dispatch.best_standards_support = :builtin
 
-  config.action_mailer.default_url_options = {:host => "localhost:3000"}
+  config.action_mailer.default_url_options = {:host => "rusrails.dev"}
 
   # Do not compress assets
   config.assets.compress = false
