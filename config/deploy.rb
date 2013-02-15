bb968ca42288cb80c6afbcc63d8dde0299149f53
Mikhail Dieterle <MikDiet@gmail.com>
2013-02-16 02:08:44 +0300

    config for new server

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,92 +1,69 @@
 require "bundler/capistrano"
 require 'thinking_sphinx/deploy/capistrano'
-load 'deploy/assets'
 
-set :application, "rusrails"
+server "78.47.229.178", :web, :app, :db, primary: true
 
-default_run_options[:pty] = true  # Must be set for the password prompt from git to work
-set :repository, "git@github.com:morsbox/rusrails.git"  # Your clone URL
-set :branch, "master"
-set :scm, "git"
+set :application, "rusrails_v32"
+set :user, 'admin'
+set :deploy_to, "/home/#{user}/apps/#{application}"
 set :deploy_via, :remote_cache
-
-dpath = "/home/hosting_mik-die/projects/rusrails"
-
-set :user, "hosting_mik-die"
-set :host, "hydrogen.locum.ru"
-
 set :use_sudo, false
-set :deploy_to, dpath
-set :bundle_cmd, 'rvm use 1.9.3-p125 do bundle'
-set :bundle_flags, "--deployment"
 
-role :web, "hydrogen.locum.ru"
-role :app, "hydrogen.locum.ru"
-role :db,  "hydrogen.locum.ru", :primary => true
+set :scm, :git
+set :repository, "git@github.com:morsbox/rusrails.git"
+set :branch, "v3.2"
 
-set :unicorn_conf, "/etc/unicorn/rusrails.mik-die.rb"
-set :unicorn_pid, "/var/run/unicorn/rusrails.mik-die.pid"
-set :unicorn_start_cmd, "(cd #{dpath}/current; rvm use 1.9.3-p125 do bundle exec unicorn_rails -Dc #{unicorn_conf})"
+default_run_options[:pty] = true
+ssh_options[:forward_agent] = true
 
-# - for unicorn - #
 namespace :deploy do
-  task :copy_configuration do
-    run "cp #{shared_path}/config/database.yml #{current_release}/config/database.yml"
-    run "cp #{shared_path}/config/production.yml #{current_release}/config/settings/production.yml"
-  end
-  before "deploy:assets:precompile", "deploy:copy_configuration"
-
-  task :migrate, :roles => :db do
-    rake = fetch(:rake, "rake")
-    rails_env = fetch(:rails_env, "production")
-    migrate_env = fetch(:migrate_env, "")
-    migrate_target = fetch(:migrate_target, :latest)
-
-    directory = case migrate_target.to_sym
-      when :current then current_path
-      when :latest then current_release
-      else raise ArgumentError, "unknown migration target #{migrate_target.inspect}"
+  %w[start stop restart].each do |command|
+    desc "#{command} unicorn server"
+    task command, roles: :app, except: {no_release: true} do
+      run "/etc/init.d/unicorn_#{application} #{command}"
     end
-
-    puts "#{migrate_target} => #{directory}"
-    run "cd #{directory}; #{rake} RAILS_ENV=#{rails_env} #{migrate_env} db:migrate"
   end
 
-  after "deploy:symlink", "deploy:migrate"
+  task :setup_config, roles: :app do
+    sudo "ln -nfs #{current_path}/config/nginx.conf /etc/nginx/sites-enabled/#{application}"
+    sudo "ln -nfs #{current_path}/config/unicorn_init.sh /etc/init.d/unicorn_#{application}"
+    run "mkdir -p #{shared_path}/config"
+    put File.read("config/database.yml"), "#{shared_path}/config/database.yml"
+    put File.read("config/settings.local.yml"), "#{shared_path}/config/settings.yml"
+    puts "Now edit the config files in #{shared_path}."
+  end
+
+  after "deploy:setup", "deploy:setup_config"
+
+  task :symlink_config, roles: :app do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+    run "ln -nfs #{shared_path}/config/settings.yml #{release_path}/config/settings/production.yml"
+  end
+
+  after "deploy:finalize_update", "deploy:symlink_config"
+
+  desc "Make sure local git is in sync with remote."
+  task :check_revision, roles: :web do
+    unless `git rev-parse HEAD` == `git rev-parse origin/master`
+      puts "WARNING: HEAD is not the same as origin/master"
+      puts "Run `git push` to sync changes."
+      exit
+    end
+  end
 
   desc "Seed the database with the required data"
   task :seed do
     rake = fetch(:rake, "rake")
     rails_env = fetch(:rails_env, "production")
 
-    run "cd #{current_release}; #{rake} RAILS_ENV=#{rails_env} db:seed"
-    run "cd #{current_release}; #{rake} RAILS_ENV=#{rails_env} pages:import"
-  end
-  # after "deploy:migrate", "deploy:seed"
-
-  desc "Start application"
-  task :start, :roles => :app do
-    run unicorn_start_cmd
+    run "cd #{release_path}; #{rake} RAILS_ENV=#{rails_env} db:seed"
+    run "cd #{release_path}; #{rake} RAILS_ENV=#{rails_env} pages:import"
+    run "cd #{release_path}; #{rake} RAILS_ENV=#{rails_env} pages:cleanup"
   end
 
-  desc "Stop application"
-  task :stop, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -QUIT `cat #{unicorn_pid}`"
-  end
-
-  desc "Restart Application"
-  task :restart, :roles => :app do
-    run "[ -f #{unicorn_pid} ] && kill -USR2 `cat #{unicorn_pid}` || #{unicorn_start_cmd}"
-  end
-
-  task :restart_ts, :roles => :app do
-    thinking_sphinx.rebuild
-  end
-
-  after "deploy:migrate", "deploy:restart_ts"
-  after "deploy:seed", "deploy:restart_ts"
+  after "deploy:migrate", "thinking_sphinx:rebuild"
+  after "deploy:seed", "thinking_sphinx:rebuild"
 end
 
-set :keep_releases, 3
-
+before "deploy", "deploy:check_revision"
 after "deploy", "deploy:cleanup"
