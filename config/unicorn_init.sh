c60c1a18707625d6034709b1a7a83d8653d72b77
Mikhail Dieterle <MikDiet@gmail.com>
2013-02-16 13:29:31 +0300

    set bundle path

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -5,7 +5,7 @@ set -e
 TIMEOUT=${TIMEOUT-60}
 APP_ROOT=/home/admin/apps/rusrails_v32/current
 PID=$APP_ROOT/tmp/pids/unicorn.pid
-CMD="cd $APP_ROOT; bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
+CMD="cd $APP_ROOT; /home/admin/.rbenv/shims/bundle exec unicorn -D -c $APP_ROOT/config/unicorn.rb -E production"
 AS_USER=admin
 set -u
 
