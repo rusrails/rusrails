a9599642133b9057160e4f6a667b09d5d1471ae6
Mikhail Dieterle <MikDiet@gmail.com>
2013-03-02 02:06:55 +0300

    add tags for unicorn script

--- config/unicorn_init.sh
+++ config/unicorn_init.sh
@@ -1,4 +1,14 @@
 #!/bin/sh
+### BEGIN INIT INFO
+# Provides:          unicorn
+# Required-Start:    $remote_fs $syslog
+# Required-Stop:     $remote_fs $syslog
+# Default-Start:     2 3 4 5
+# Default-Stop:      0 1 6
+# Short-Description: unicorn
+# Description:       unicorn
+### END INIT INFO
+
 set -e
 
 # Feel free to change any of the following variables for your app:
