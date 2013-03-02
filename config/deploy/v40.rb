cd805b8d0dd966a9ae7cf8a2ec372ed431ca545a
Mikhail Dieterle <MikDiet@gmail.com>
2013-03-02 19:51:00 +0300

    multistaging

---
+++ config/deploy/v40.rb
@@ -0,0 +1,5 @@
+set :application, "rusrails_v40"
+
+set :branch, "master"
+
+set :subdomains, %w(edge.rusrails.ru v40.rusrails.ru)
--- config/unicorn.rb
+++
@@ -1,9 +0,0 @@
-root = "/home/admin/apps/rusrails_v40/current"
-working_directory root
-pid "#{root}/tmp/pids/unicorn.pid"
-stderr_path "#{root}/log/unicorn.log"
-stdout_path "#{root}/log/unicorn.log"
-
-listen "/tmp/unicorn.rusrails_v40.sock"
-worker_processes 2
-timeout 30
