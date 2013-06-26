517143fc8d8aa76b2d0c2c7d8622272e50f396ce
Mikhail Dieterle <MikDiet@gmail.com>
2013-06-26 03:22:44 +0300

    prepare release

--- config/deploy/v32.rb
+++ config/deploy/v32.rb
@@ -1,4 +1,4 @@
 set :application, "rusrails_v32"
 set :branch, "v3.2"
 
-set :subdomains, %w(rusrails.ru www.rusrails.ru v32.rusrails.ru)
+set :subdomains, %w(v32.rusrails.ru)
--- config/deploy/v40.rb
+++ config/deploy/v40.rb
@@ -2,4 +2,4 @@ set :application, "rusrails_v40"
 
 set :branch, "master"
 
-set :subdomains, %w(edge.rusrails.ru v40.rusrails.ru)
+set :subdomains, %w(rusrails.ru www.rusrails.ru edge.rusrails.ru v40.rusrails.ru)
