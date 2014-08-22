ecb85d97958abdc8e088ac5c421438167e766cb9
Mikdiet <mikdiet@gmail.com>
2014-08-22 18:05:25 +0300

    update

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source 'https://rubygems.org'
 
-gem 'rails', " ~> 4.0.0"
+gem 'rails', " ~> 4.1.5"
 gem 'russian'
 gem 'static_docs' #, path: "/Users/mikdiet/projects/opensource/static_docs"
 
--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -2,6 +2,8 @@ ENV["RAILS_ENV"] ||= 'test'
 require File.expand_path("../../config/environment", __FILE__)
 require 'rspec/rails'
 
+ActiveRecord::Migration.maintain_test_schema!
+
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
 Dir[Rails.root.join("spec/support/**/*.rb")].each {|f| require f}
