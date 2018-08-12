e7c5847d39f395b19e1b5c3faf675f724a615919
Mikhail Dieterle <mikdiet@gmail.com>
2018-08-12 12:42:26 +0300

    Use master for new server

--- config/deploy/production.rb
+++ config/deploy/production.rb
@@ -2,6 +2,6 @@ load "config/recipes/postgresql"
 
 set :application, "rusrails"
 
-set :branch, "md/new-server-2"
+set :branch, "master"
 
 set :subdomains, %w(new.rusrails.ru rusrails.ru www.rusrails.ru)
