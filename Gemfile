4c783b0cc261d0a89494d664e69cc9bb4a4ec52d
Mikhail <mikdiet@gmail.com>
2021-05-15 21:36:44 +0300

    Upgrade ruby

--- Gemfile
+++ Gemfile
@@ -1,6 +1,5 @@
 source 'https://rubygems.org'
-ruby '2.4.1'
-gem 'bundler', '~> 2.1'
+ruby '2.6.6'
 
 gem 'rails', " ~> 5.2.4.5"
 gem 'russian'
--- config/recipes/ruby.rb
+++ config/recipes/ruby.rb
@@ -1,11 +1,11 @@
-set(:ruby_version, "2.4.1")
+set(:ruby_version, "2.6.6")
 set(:bundle_cmd) { "bundle" }
 set(:bundle_flags) { '--deployment' }
 
 namespace :ruby do
   desc "Install everything onto the server"
   task :install do
-    run "#{sudo} apt-get -y install gcc libc6-dev libv8-dev build-essential libssl-dev libreadline-dev zlib1g-dev ca-certificates autoconf automake bison libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev pkg-config sqlite3 libgmp-dev"
+    run "#{sudo} apt-get -y install gcc libc6-dev build-essential libssl-dev libreadline-dev zlib1g-dev ca-certificates autoconf automake bison libffi-dev libgdbm-dev libncurses5-dev libsqlite3-dev libtool libyaml-dev pkg-config sqlite3 libgmp-dev"
     run "gpg --keyserver hkp://keys.gnupg.net --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB"
     run "curl -sSL https://get.rvm.io | bash -s stable"
     run "source ~/.rvm/scripts/rvm"
