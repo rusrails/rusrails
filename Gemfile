b732b0ba115f2f623ffefbb351ab5b0c448680ec
Mikhail <mikdiet@gmail.com>
2021-05-16 17:32:15 +0300

    Upgrade to Rails 6.0

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 ruby '2.6.6'
 
-gem 'rails', " ~> 5.2.4.5"
+gem 'rails', " ~> 6.0.3"
 gem 'russian'
 gem 'static_docs' #, path: "/Users/mikdiet/projects/opensource/static_docs"
 gem "bootsnap", ">= 1.1.0", require: false
--- db/schema.rb
+++ db/schema.rb
@@ -1,39 +1,37 @@
-# encoding: UTF-8
 # This file is auto-generated from the current state of the database. Instead
 # of editing this file, please use the migrations feature of Active Record to
 # incrementally modify your database, and then regenerate this schema definition.
 #
-# Note that this schema.rb definition is the authoritative source for your
-# database schema. If you need to create the application database on another
-# system, you should be using db:schema:load, not running all the migrations
-# from scratch. The latter is a flawed and unsustainable approach (the more migrations
-# you'll amass, the slower it'll run and the greater likelihood for issues).
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20130503124427) do
+ActiveRecord::Schema.define(version: 2021_05_16_125901) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "redirects", force: true do |t|
-    t.string   "from"
-    t.string   "to"
+  create_table "redirects", force: :cascade do |t|
+    t.string "from"
+    t.string "to"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
 
-  create_table "static_docs_pages", force: true do |t|
-    t.string   "title"
-    t.string   "path"
-    t.string   "namespace"
-    t.text     "body"
-    t.string   "extension"
+  create_table "static_docs_pages", force: :cascade do |t|
+    t.string "title"
+    t.string "path"
+    t.string "namespace"
+    t.text "body"
+    t.string "extension"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.index ["namespace"], name: "index_static_docs_pages_on_namespace"
+    t.index ["path"], name: "index_static_docs_pages_on_path"
   end
-
-  add_index "static_docs_pages", ["namespace"], name: "index_static_docs_pages_on_namespace", using: :btree
-  add_index "static_docs_pages", ["path"], name: "index_static_docs_pages_on_path", using: :btree
 
 end
